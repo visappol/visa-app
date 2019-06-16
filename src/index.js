514855d0f60070755494ff491af7308c468887cf
Sunandan Dhawan <sunandan94@gmail.com>
2019-06-16 18:45:44 +0530

    get port from env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8080; // default port to listen
+const port = process.env.PORT || 8080; // default port to listen
 app.use(express.static("views"));
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
